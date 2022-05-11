1fb1fffa1462f646b2f93631c80da0a9a7b3d765
eliseprts <elisepourtois.pro@gmail.com>
2022-05-11 11:59:59 +0200

    part 2 chapter 2 and 3

--- shiny-agency/src/index.jsx
+++ shiny-agency/src/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { ThemeProvider } from './utils/context/context'
+import { ThemeProvider, SurveyProvider } from './utils/context/context'
 
 import Header from './components/Header'
 import Home from './pages/Home'
@@ -19,17 +19,19 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
     <ThemeProvider>
-      <GlobalStyle />
-      <Header />
-      <Routes>
-          <Route exact path='/' element={<Home/>}/>
-          <Route path='/survey/:questionNumber' element={<Survey/>}/>
-          <Route path='/results' element={<Results/>}/>
-          <Route path='/freelances' element={<Freelances/>}/>
-          {/* Route (with path='*') which send back to error page if the path in the URL has no matching, to place à the end of <Routes/> */}
-          <Route path='*' element={<Error/>}/> 
-      </Routes>
-      <Footer />
+      <SurveyProvider>
+        <GlobalStyle />
+        <Header />
+        <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route path='/survey/:questionNumber' element={<Survey/>}/>
+            <Route path='/results' element={<Results/>}/>
+            <Route path='/freelances' element={<Freelances/>}/>
+            {/* Route (with path='*') which send back to error page if the path in the URL has no matching, to place à the end of <Routes/> */}
+            <Route path='*' element={<Error/>}/> 
+        </Routes>
+        <Footer />
+      </SurveyProvider>
     </ThemeProvider>
   </BrowserRouter>
 )
--- shiny-agency/src/pages/Survey.jsx
+++ shiny-agency/src/pages/Survey.jsx
@@ -1,5 +1,8 @@
 import { Link, useParams } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import { useContext } from 'react'
+
+import { SurveyContext } from '../utils/context/context'
+import { useFetch } from '../utils/hooks/hooks'
 
 import styled from 'styled-components'
 import colors from '../utils/style/colors'
@@ -27,64 +30,83 @@ const LinkWrapper = styled.div`
         margin-right: 20px;
     }
 `
+const ReplyBox = styled.button`
+    border: non;
+    height: 100px;
+    width: 300px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: ${colors.backgroundLight};
+    border-radius: 30px;
+    cursor: pointer;
+    box-shadow: ${(props) =>
+        props.isSelected ? `0px 0px 0px 2px ${colors.primary} inset` : 'none'};
+    $:first-child {
+        margin-right: 15px;
+    }
+    &:last-of-type {
+        margin-left: 15px;
+    }
+`
+const ReplyWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+`
 
-
-function Survey () {
-    // useParams() recovers and passes parameters through a route
-    const {questionNumber} = useParams()
+function Survey() {
+    const { questionNumber } = useParams() // useParams() recovers and passes parameters through a route
     const questionNumberInt = parseInt(questionNumber)
     const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
     const nextQuestionNumber = questionNumberInt + 1
-    // Variables for recovering questions from API
-    const [surveyData, setSurveyData] = useState([])
-    // Loader
-    const [isDataLoading, setDataLoading] = useState(false)
-    // Error
-    const [error, setError] = useState(false)
-    
-    // Recover questions from API
-    useEffect(() => {
-        async function fetchSurvey() {
-            setDataLoading(true) // Display loader
-            try{
-                const response = await fetch(`http://localhost:8000/survey`)
-                const {surveyData} = await response.json()
-                setSurveyData(surveyData)
-            }
-            catch(err) {
-                console.log(err)
-                setError(true)
-            }
-            finally {
-                setDataLoading(false) // Hide loader
-            }
-        }
-        fetchSurvey()
-    }, [])
-
-    if (error) {
-        return <span>Oups il y a eu un problème</span>
+  
+    // Save answer
+    const { saveAnswers, answers } = useContext(SurveyContext)
+    function saveReply(answer) {
+      saveAnswers({ [questionNumber]: answer })
     }
-
+  
+    // Recovering questions from API with custom hook useFetch
+    const { data, isLoading, error } = useFetch(`http://localhost:8000/survey`)
+    const { surveyData } = data
+  
+    if (error) {
+      return <span>Oups il y a eu un problème...</span>
+    }
+  
     // Return page
     return (
-        <SurveyContainer>
-            <QuestionTitle>Question {questionNumber}</QuestionTitle>
-            {isDataLoading ? (
-                <Loader />
-            ) : (
-                <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
-            )}
-            <LinkWrapper>
-                <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-                {surveyData[questionNumberInt + 1] ? (
-                    <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
-                ) : (
-                    <Link to='/results'>Résultats</Link>
-                )}
-            </LinkWrapper>
-        </SurveyContainer>
+      <SurveyContainer>
+        <QuestionTitle>Question {questionNumber}</QuestionTitle>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <QuestionContent>
+            {surveyData && surveyData[questionNumber]}
+          </QuestionContent>
+        )}
+        <ReplyWrapper>
+          <ReplyBox
+            onClick={() => saveReply(true)}
+            isSelected={answers[questionNumber] === true}>
+            Oui
+          </ReplyBox>
+          <ReplyBox
+            onClick={() => saveReply(false)}
+            isSelected={answers[questionNumber] === false}>
+            Non
+          </ReplyBox>
+        </ReplyWrapper>
+        <LinkWrapper>
+          <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+          {surveyData && surveyData[questionNumberInt + 1] ? (
+            <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+          ) : (
+            <Link to="/results">Résultats</Link>
+          )}
+        </LinkWrapper>
+      </SurveyContainer>
     )
-}
+  }
 
 export default Survey
